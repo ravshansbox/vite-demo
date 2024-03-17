d4ba483330fb5b0d4c5f3f50d906940cf65a55bb
Ravshan Samandarov <ravshansbox@gmail.com>
2024-03-17 15:16:18 +0300

    initial commit

---
+++ src/App.test.jsx
@@ -0,0 +1,8 @@
+import { App } from './App';
+import { render, screen } from '@testing-library/react';
+
+test('App component renders the text "App"', () => {
+  render(<App />);
+
+  expect(screen.getByText('App')).toBeInTheDocument();
+});
---
+++ vite.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [viteReact()],
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+    setupFiles: ['./src/vitest-setup.js'],
+  },
+});
