ed34407f2ad2e57e91392202437d4c6f99c56941
Ravshan Samandarov <ravshansbox@gmail.com>
2024-10-04 18:09:13 +0300

    add usage example for `createSlice`

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,50 @@
-import classes from './App.module.css';
+import { useReducer, useState } from 'react';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  level1: {
+    level2: {
+      recipes: [{ id: 1, name: 'Pancakes' }],
+    },
+  },
+};
+
+const { reducer, actions } = createSlice({
+  name: 'recipes',
+  initialState,
+  reducers: {
+    addRecipe: (state, { payload }) => {
+      const id = Math.max(...state.level1.level2.recipes.map((r) => r.id)) + 1;
+      state.level1.level2.recipes.push({ id, name: payload.name });
+    },
+    updateName: (state, { payload }) => {
+      state.level1.level2.recipes[payload.index].name = payload.newName;
+    },
+  },
+});
 
 export const App = () => {
-  return <h1 className={classes.heading}>Hello World</h1>;
+  const [name, setName] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <main>
+      <ul>
+        {state.level1.level2.recipes.map((recipe, index) => (
+          <li>
+            <input
+              value={recipe.name}
+              onChange={(e) =>
+                dispatch(actions.updateName({ index, newName: e.target.value }))
+              }
+            />
+          </li>
+        ))}
+      </ul>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => dispatch(actions.addRecipe({ name }))}>
+        Create
+      </button>
+    </main>
+  );
 };
